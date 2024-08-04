0bf34e824b45c33ebbce18de203d87a9d453480a
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-08-04 18:41:28 +0000

    Add home-manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,15 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      # Keep ``inputs.nixpkgs`` of home-manager consistent with
+      # the ``inputs.nixpkgs`` of the current flake and thus avoid
+      # problems caused by having different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -10,6 +18,12 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+	inputs.home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.bhubache = import ./home.nix;
+	}
       ];
     };
   };
