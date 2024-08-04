c2a68c1663cd053c4f02528275cba2e6886fdf1b
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-08-04 18:13:52 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
