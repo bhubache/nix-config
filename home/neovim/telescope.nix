c7bbfc80d1d0bf6fe647ec8788d207e5aac210a9
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-08-09 22:17:11 +0000

    Add telescope keymaps

--- home/neovim/telescope.nix
+++ home/neovim/telescope.nix
@@ -3,7 +3,25 @@
     enable = true;
 
     keymaps = {
-      "<leader>fg" = "live_grep";
+      "<leader>sg" = {
+        action = "live_grep";
+	options = {
+	  desc = "Grep (root dir)";
+	};
+      };
+      "<leader>sf" = {
+        action = "find_files";
+	options = {
+	  desc = "Find project files";
+	};
+      };
+      "<leader>sb" = {
+        action = "buffers";
+	options = {
+	  desc = "Find file among buffers";
+	};
+      };
+      # TODO: Search all buffers
     };
 
     extensions.fzf-native = { enable = true; };
