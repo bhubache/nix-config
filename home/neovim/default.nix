94426555d9ed32470638bca5960be11c8a674f66
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-08-09 21:46:43 +0000

    Fix transparency of Telescope

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -17,6 +17,25 @@
 
     plugins.transparent = {
       enable = true;
+      settings = {
+        extra_groups = [
+          "NormalFloat"
+
+	  "TelescopePromptTitle"
+	  "TelescopePromptPrefix"
+	  "TelescopePromptCounter"
+	  "TelescopePromptNormal"
+	  "TelescopePromptBorder"
+
+	  "TelescopeResultsTitle"
+	  "TelescopeResultsNormal"
+	  "TelescopeResultsBorder"
+
+	  "TelescopePreviewTitle"
+	  "TelescopePreviewNormal"
+	  "TelescopePreviewBorder"
+        ];
+      };
     };
   };
 }
