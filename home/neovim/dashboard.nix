3230cd24abab18b834ec40fa02b49b6637cecb79
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-08-10 00:39:42 +0000

    Remove commented out code

--- home/neovim/dashboard.nix
+++ home/neovim/dashboard.nix
@@ -27,42 +27,30 @@
 	  {
 	    action = "Telescope find_files cwd=";
 	    desc = "Find File                                    ";
-	    # desc_hl = "String";
 	    icon = "  ";
-	    # icon_hl = "Title";
 	    key = "f";
 	    key_format = "%s";
-	    # key_hl = "Number";
 	  }
 	  {
 	    action = ":ene <BAR> startinsert <CR>";
 	    desc = "New File                                     ";
-	    # desc_hl = "String";
 	    icon = "  ";
-	    # icon_hl = "Title";
 	    key = "n";
 	    key_format = "%s";
-	    # key_hl = "Number";
 	  }
 	  {
 	    action = "Telescope live_grep";
 	    desc = "Find Text                                    ";
-	    # desc_hl = "String";
 	    icon = "  ";
-	    # icon_hl = "Title";
 	    key = "g";
 	    key_format = "%s";
-	    # key_hl = "Number";
 	  }
 	  {
 	    action = ":qa";
 	    desc = "Quit                                         ";
-	    # desc_hl = "String";
 	    icon = "  ";
-	    # icon_hl = "Title";
 	    key = "q";
 	    key_format = "%s";
-	    # key_hl = "Number";
 	  }
 	];
       };
