03337381d81774fa401cfec05f6afc6cb60417de
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-08-18 16:21:33 -0500

    Add lualine and vim split navigation

--- home/bhubache/common/core/neovim/default.nix
+++ home/bhubache/common/core/neovim/default.nix
@@ -9,6 +9,7 @@
     ./dashboard.nix
     ./transparent.nix
     ./cmp.nix
+    ./lualine.nix
   ];
 
   programs.nixvim = {
@@ -17,5 +18,24 @@
 
     enable = true;
     defaultEditor = true;
+
+    keymaps = [
+      {
+        action = "<cmd>wincmd l<CR>";
+	key = "<C-l>";
+      }
+      {
+        action = "<cmd>wincmd h<CR>";
+	key = "<C-h>";
+      }
+      {
+        action = "<cmd>wincmd j<CR>";
+	key = "<C-j>";
+      }
+      {
+        action = "<cmd>wincmd k<CR>";
+	key = "<C-k>";
+      }
+    ];
   };
 }
