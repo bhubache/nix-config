d0defe405439ad1e3170c73dacc0bc66ba7f1bc2
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-08-18 13:07:46 -0500

    Add border to which-key

--- home/bhubache/common/core/neovim/transparent.nix
+++ home/bhubache/common/core/neovim/transparent.nix
@@ -21,6 +21,8 @@
         
         "Pmenu"
         "CmpItemAbbr"
+
+	"WhichKeyBorder"
       ];
     };
   };
--- home/bhubache/common/core/neovim/which-key.nix
+++ home/bhubache/common/core/neovim/which-key.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   programs.nixvim.plugins.which-key = {
     enable = true;
+    window.border = "rounded";
   };
 }
