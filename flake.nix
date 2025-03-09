dbbf1ec5ca9b5393968d850b047b5d8246fd5c79
bhubache <brandon.hubacher@gmail.com>
2025-03-08 21:31:05 -0600

    Update from 24.05 to 24.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       # Keep ``inputs.nixpkgs`` of home-manager consistent with
       # the ``inputs.nixpkgs`` of the current flake and thus avoid
       # problems caused by having different versions of nixpkgs.
@@ -13,7 +13,7 @@
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.05";
+      url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
