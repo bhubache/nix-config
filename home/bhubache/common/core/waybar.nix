06b08288d9db8bc20c018cea5f291bf89b6b85fc
bhubache <brandon.hubacher@gmail.com>
2025-03-09 11:40:51 -0500

    Improve waybar

--- home/bhubache/common/core/waybar.nix
+++ home/bhubache/common/core/waybar.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
   programs.waybar = {
     enable = true;
+    style = ''
+      * {
+        border: none;
+	border-radius: 0;
+	font-family: "JetBrains Mono";
+      }
+    '';
     settings = {
       mainBar = {
         layer = "top";
@@ -10,16 +17,27 @@
 	modules-left = [ "sway/workspaces" ];
 	modules-center = [ "clock" ];
 	modules-right = [ "pulseaudio" "memory" "cpu" "disk" "battery" ];
+	spacing = 10;
+	"sway/workspaces" = {
+	  persistent-workspaces = {
+	    "1" = ["eDP-1"];
+	    "2" = ["eDP-2"];
+	    "3" = ["eDP-3"];
+	  };
+	};
         clock = {
 	  interval = 1;
-          format = "{:%r}";  # TODO: Link for syntax
+          format = "{:%R %p}";
+
+	  # NOTE: https://github.com/Alexays/Waybar/issues/3024
+	  timezone = "America/North_Dakota/Center";
         };
 	pulseaudio = {
-	  format = "{volume}% {icon} {format_source}";
+	  format = "VOL {volume}%    MIC {format_source}";
 	  format-bluetooth = "{volume}% {icon} {format_source}";
     	  format-bluetooth-muted = "󰝟 {icon} {format_source}";
           format-muted = "󰝟  {format_source}";
-          format-source = "{volume}% ";
+          format-source = "{volume}%";
           format-source-muted = "";
           format-icons = {
             headphone = "";
@@ -32,10 +50,10 @@
           };
 	};
 	memory = {
-	  format = "{}% Memory";
+	  format = "RAM {}%";
 	};
 	cpu = {
-	  format = "{usage}% CPU";
+	  format = "CPU {usage}%";
 	};
 	disk = {
 	  interval = 30;
