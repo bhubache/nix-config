7e23b071603b2e88bc936a551469142b6e16395b
bhubache <brandon.hubacher@gmail.com>
2025-03-09 13:06:39 -0500

    Configure btop

--- home/bhubache/common/core/btop.nix
+++ home/bhubache/common/core/btop.nix
@@ -1,5 +1,10 @@
-{pkgs, ...}: {
+{pkgs, lib, ...}: {
   programs.btop = {
     enable = true;
+    settings = {
+      color_theme = lib.mkForce "onedark";
+      base_10_sizes = true;
+      temp_scale = "fahrenheit";
+    };
   };
 }
