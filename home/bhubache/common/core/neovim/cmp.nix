e8a886416e2215a85aa9a6816efe3f772a0d5b3a
bhubache <brandon.hubacher@gmail.com>
2025-06-01 21:27:35 -0500

    Fix cmp navigation keybind (from colemak)

--- home/bhubache/common/core/neovim/cmp.nix
+++ home/bhubache/common/core/neovim/cmp.nix
@@ -12,7 +12,7 @@
       ];
       mapping = {
         "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-        "<C-e>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+        "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         "<CR>" = "cmp.mapping.confirm({ select = true })";
         # Pull up menu
         "<C-k>" = "cmp.mapping.complete()";
