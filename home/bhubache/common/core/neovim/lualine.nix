7b035e4fc540de7e817e1f2405eac7d10798d9d5
bhubache <brandon.hubacher@gmail.com>
2025-09-28 20:40:28 -0500

    Hardcode neovim status line theme due to likely
    stylix issue

--- home/bhubache/common/core/neovim/lualine.nix
+++ home/bhubache/common/core/neovim/lualine.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
   programs.nixvim.plugins.lualine = {
     enable = true;
+
+    # FIXME: This is here because it seems that stylix isn't properly
+    # 	     configuring the theme.
+    settings.options.theme = "onedark";
   };
 }
