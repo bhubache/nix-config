946e76b2915087505bc9bfd8dcf11ff772272f29
bhubache <brandon.hubacher@gmail.com>
2025-10-19 15:57:00 -0500

    Add note regarding potential wifi issues

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -76,6 +76,10 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
+  # NOTE: If you see intermittent wifi connection issues, uncomment the
+  # 	  following line.
+  # networking.networkmanager.wifi.powersave = false;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
