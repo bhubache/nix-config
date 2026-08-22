f91d660bd3cd64f68e1be83d25ad55897ae0137d
bhubache <brandon.hubacher@gmail.com>
2026-08-22 18:27:26 -0500

    Specify font for obsidian

--- home/bhubache/common/core/obsidian.nix
+++ home/bhubache/common/core/obsidian.nix
@@ -8,6 +8,11 @@
       enable = true;
       target = "obsidian/Everything";
       settings = {
+        appearance = {
+          interfaceFontFamily = "JetBrainsMono Nerd Font";
+          monospaceFontFamily = "JetBrainsMono Nerd Font";
+        };
+
         communityPlugins = with pkgs.obsidianPlugins; [
           codeblock-customizer
         ];
